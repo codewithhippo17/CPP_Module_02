433f86bb8b2a6b9d5ca524bc8d84c5188c038f2e
ehamza <ehamza@student.1337.ma>
2026-01-12 21:25:27 +0100

    change the message being displayed on destructor trigring

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -17,7 +17,7 @@ Fixed &Fixed::operator=(const Fixed &other) {
   return (*this);
 }
 
-Fixed::~Fixed() { std::cout << "Default constructor called" << std::endl; }
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
 
 int Fixed::getRawBits(void) const {
   std::cout << "getRawBits member function called" << std::endl;
